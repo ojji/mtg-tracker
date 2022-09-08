c71848a4b968ab248294ca5783b43b9928791757
ojji <zshun86@gmail.com>
2022-09-08 02:02:46 +0200

    [refactor] Remove unnecessary use import

--- injector/src/assembler.rs
+++ injector/src/assembler.rs
@@ -1,5 +1,3 @@
-use std::fmt::write;
-
 /// Implementation of the Assembler was made possible by
 /// [this amazing Online x86 / x64 Assembler and Disassembler](https://defuse.ca/online-x86-assembler.htm)
 ///
