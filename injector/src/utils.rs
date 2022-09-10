603d7d5ae5ae5d5f36e448dd25573c71b18ea6c5
ojji <zshun86@gmail.com>
2022-09-10 11:32:15 +0200

    [style] Indentation change

--- injector/src/utils.rs
+++ injector/src/utils.rs
@@ -1,6 +1,6 @@
 pub(crate) fn get_process_name(process_name_raw: &[u16]) -> String {
-  let len = process_name_raw.iter().take_while(|&&c| c != 0).count();
-  String::from_utf16(&process_name_raw[..len]).unwrap()
+    let len = process_name_raw.iter().take_while(|&&c| c != 0).count();
+    String::from_utf16(&process_name_raw[..len]).unwrap()
 }
 
 pub(crate) fn get_module_name(raw_module_name: &[u16]) -> String {
